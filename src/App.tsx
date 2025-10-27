a1522e94727059e5f01d10e77eee9bc3071118dc
Abdelrahman Sayed <110208261+Abdelrahman0963@users.noreply.github.com>
2025-10-27 19:18:51 +0300

    save local changes before pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes.tsx'
@@ -7,17 +6,8 @@ function App() {
 
   return (
     <>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
-=======
-import React from 'react'
-
-const App: React.FC = () => {
-  return (
-    <div>
-
-    </div>
->>>>>>> cede7134efc2650f26f667cfbc5aec13ac84348f
   )
 }
 
