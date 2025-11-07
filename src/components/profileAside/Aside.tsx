3d5247d5c7bc954797ccd7ef52517a92dcdc5b5c
Abdelrahman Sayed <110208261+Abdelrahman0963@users.noreply.github.com>
2025-11-07 04:14:01 +0200

    update

--- src/components/profileAside/Aside.tsx
+++ src/components/profileAside/Aside.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
-import { NavLink } from 'react-router'
+import { NavLink, useNavigate } from 'react-router'
 import UserInfo from './UserInfo'
+import Cookies from 'js-cookie'
+import type { AppDispatch } from "@/store/Store";
+import { useDispatch } from 'react-redux'
+import { logout } from '@/services/auth/Auth'
 
 const Aside: React.FC = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch<AppDispatch>();
+
+    const handleLogout = async () => {
+        const refreshToken = Cookies.get("refreshToken");
+        if (!refreshToken) return;
+
+        try {
+            await dispatch(logout({ refreshToken })).unwrap();
+        } catch (err) {
+            console.error("Logout error:", err);
+        } finally {
+            Cookies.remove("refreshToken");
+            Cookies.remove("accessToken");
+            navigate("/login");
+            window.location.reload();
+        }
+    };
+
 
     return (
         <aside className='flex flex-col items-center gap-12 h-auto w-full md:w-[25%]  bg-[#F5F6F7] rounded-lg py-16! px-4! '>
@@ -20,7 +43,7 @@ const Aside: React.FC = () => {
                 <NavLink to="/privacy" className='flex items-center gap-2  border border-[#6D7379] p-2! rounded-lg'><img src="/icons/profile/Lock Keyhole Minimalistic.svg" alt="lock icon" loading="lazy" />
                     <p>Privacy Policy</p>
                 </NavLink>
-                <button className='flex items-center  gap-2 p-2"'>
+                <button onClick={handleLogout} className='flex items-center  gap-2 p-2"'>
                     <img src="/icons/profile/Logout 4.svg" alt="Logout icon" loading="lazy" />
                     <p className='text-[#FC4B4E]'>Logout</p>
                 </button>
--- src/services/auth/Auth.ts
+++ src/services/auth/Auth.ts
@@ -152,12 +152,16 @@ export const refreshToken = createAsyncThunk(
         }
     }
 );
+interface logoutForm {
+    refreshToken: string
+
+}
 
 export const logout = createAsyncThunk(
     "user/logout",
-    async (_, { rejectWithValue }) => {
+    async (data: logoutForm, { rejectWithValue }) => {
         try {
-            const response = await axios.post(`${base_url}api/Identity/Accounts/logout`, null, {
+            const response = await axios.post(`${base_url}api/Identity/Accounts/logout`, data, {
                 headers: {
                     "Content-Type": "application/json",
                 },
@@ -245,7 +249,31 @@ const userSlice = createSlice({
             .addCase(googleLogin.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload as string | null;
-            });
+            })
+            .addCase(refreshToken.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(refreshToken.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload.user;
+                state.error = null;
+            })
+            .addCase(refreshToken.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload as string | null;
+            })
+            .addCase(logout.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(logout.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = null;
+                state.error = null;
+            })
+            .addCase(logout.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload as string | null;
+            })
     },
 });
 
